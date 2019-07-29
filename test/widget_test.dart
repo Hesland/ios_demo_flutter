bf19c5b1bbc88e0bda657c1d94306d113a069333
Hesland <317914715@qq.com>
2019-07-29 16:12:16 +0800

    setup navigation bar items

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:ios_demo_flutter/main.dart';
+import '../lib/views/example_view.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
